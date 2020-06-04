52415993eab4e37e383e5b75a6615a517cc83bb1
ihe618 <ihe618@users.noreply.github.com>
2020-06-04 18:36:52 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -4,34 +4,34 @@
 
 int main(void)
 {
-  float number;                           //define number
-  do
-  {
-  number = get_float("Change owed: $"); 
-  }
-  while (number<0);                                 //get positive number
- int cents = round(number*100);                     //define and convert to cents
+    float number;                                     //define number
+    do
+    {
+        number = get_float("Change owed: $"); 
+    }
+    while (number < 0);                               //get positive number
+    int cents = round(number * 100);                  //define and convert to cents
 
- int counter = 0;                                   //define counter
+    int counter = 0;                                  //define counter
 
 //add number of quarters
-counter = counter + cents/25;                       //add quotient to counter (works even if smaller than 25)
+    counter = counter + cents / 25;                   //add quotient to counter (works even if smaller than 25)
    
     //if there is remainder (if no remainder- done)
-    if(cents%25)                                   
+    if (cents % 25)                                   
     {
-        int x = cents%25;                           //define x as remainder after division by 25  
-        if(x/10 >= 1)                                //if x bigger than 10
+        int x = cents % 25;                             //define x as remainder after division by 25  
+        if (x / 10 >= 1)                                //if x bigger than 10
         {
-            counter = counter + x/10;                //add quotient after division by 10   
-            int y = x%10;                            //define y as remainder after division by 10
-            counter = counter + y/5 + y%5;           //then add quotient from division by 5 and remainder (pennies)
+            counter = counter + x / 10;                 //add quotient after division by 10   
+            int y = x % 10;                             //define y as remainder after division of x by 10
+            counter = counter + y / 5 + y % 5;          //add quotient from division of y by 5 and remainder 
         }
-        else if (x/10 < 1)                           //if remainder x less than 10
+        else if (x / 10 < 1)                              //else if remainder x less than 10
         {                    
-            counter = counter + x/5 + x%5;           //then add quotient from division of x by 5 and remainder 
+            counter = counter + x / 5 + x % 5;              //add quotient from division of x by 5 and remainder 
         }
     }
 
-printf("Min coins: %i\n", counter);
+    printf("Min coins: %i\n", counter);
 }
