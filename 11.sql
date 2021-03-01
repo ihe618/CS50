05e6609c7a16d5ba5d2f46d233b3a02f04cd54b7
ihe618 <ihe618@users.noreply.github.com>
2021-03-01 02:18:59 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -1,1 +1,6 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name LIKE "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5);
+SELECT title FROM movies
+    JOIN ratings ON movies.id = ratings.movie_id
+    JOIN stars ON ratings.movie_id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
+    WHERE name = "Chadwick Boseman"
+    ORDER BY rating DESC LIMIT 5;
